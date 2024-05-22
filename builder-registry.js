dee5a071dcb23133e6dd9eeb78cd523119123706
SplitBase Shopify <access@splitbase.com>
2024-05-22 16:01:10 +0530

    add builder.io files with react

---
+++ builder-registry.js
@@ -0,0 +1,32 @@
+import { builder, Builder } from "@builder.io/react";
+import dynamic from "next/dynamic";
+
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
+
+Builder.registerComponent(
+  dynamic(() => import("./components/Counter/Counter")),
+  {
+    name: "Counter",
+  }
+);
+
+Builder.registerComponent(
+  dynamic(() => import("./pages")),
+  {
+    name: "Home Page",
+  }
+);
+
+Builder.registerComponent(
+  dynamic(() => import("./pages/[...page]")),
+  {
+    name: "Page",
+  }
+);
+
+Builder.registerComponent(
+  dynamic(() => import("./pages/_app")),
+  {
+    name: "MyApp",
+  }
+);
